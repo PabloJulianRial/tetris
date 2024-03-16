d9719ea29d47c72fd1a817f6210795000ad37538
Pablo Rial Sartirana <65549425+rialpablojhotmailcom@users.noreply.github.com>
2024-03-16 16:18:32 +0000

    add shape movement down and stop at the bottom

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,48 @@
-import './style.scss'
-import { Shape, square, stick, jey, ell, ess, zee, tee } from './shapes'
-import { shapesArray } from './shapes'
+import "./style.scss";
+import { Shape, square } from "./shapes";
+import { shapesArray } from "./shapes";
 
-const board = document.querySelector<HTMLDivElement>(".game__board")
+const board = document.querySelector<HTMLDivElement>(".game__board");
 
-
-if(!board){
-  throw new Error("cells not found")
+if (!board) {
+  throw new Error("cells not found");
 }
 
-for(let i:number = 1; i < 241; i++){
-  const cell = document.createElement("div")
-  cell.classList.add(`cell${i}`)
-  cell.classList.add(`game__board__cell`)
-  cell.innerText = `${i}`
-  cell.style.fontSize = "10px"
-  board.appendChild(cell)
+for (let i: number = 1; i < 241; i++) {
+  const cell = document.createElement("div");
+  cell.classList.add(`cell${i}`);
+  cell.classList.add(`game__board__cell`);
+  cell.innerText = `${i}`;
+  cell.style.fontSize = "10px";
+  board.appendChild(cell);
 }
-const boardCells = document.querySelectorAll<HTMLDivElement>(".game__board__cell")
+const boardCells =
+  document.querySelectorAll<HTMLDivElement>(".game__board__cell");
 
-if(!boardCells){
-  throw new Error("cells not found")
+if (!boardCells) {
+  throw new Error("cells not found");
 }
-const renderShape = (shape: Shape) :void=> {
-  shape.cells.forEach((cell) => {
-      const cellElement = boardCells[cell];
-      cellElement.style.backgroundColor = shape.color;
+const renderShape = (CurrentShape: Shape): void => {
+  CurrentShape.cells.forEach((cell) => {
+    const cellElement = boardCells[cell];
+    cellElement.style.backgroundColor = CurrentShape.color;
   });
-}
+};
 
+let CurrentShape: Shape = square;
 
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowDown") {
+    const isAtBottom = CurrentShape.cells.some((cell) => cell + 12 > 240);
 
+    if (!isAtBottom) {
+      CurrentShape.cells = CurrentShape.cells.map((cell) => cell + 12);
+    }
+    
+    boardCells.forEach((cellElement) => {
+      cellElement.style.backgroundColor = "rgb(233, 210, 210)";
+    });
 
-renderShape(square);
-renderShape(stick);
-renderShape(ell);
-renderShape(jey);
-renderShape(tee);
-renderShape(ess);
-renderShape(zee);
+    renderShape(CurrentShape);
+  }
+});
